501779862cb8886cd74d1f17e577c74a41c440a1
MatheusSuzart <mathsuzart@outlook.com>
2022-04-30 00:02:49 -0300

    route-ended

--- src/routes.js
+++ src/routes.js
@@ -9,4 +9,20 @@ router.get('/ping', (req,res)=> {
     res.json({pong: true});
 });
 
+router.get('/states', UserController.getStates);
+
+router.post('/user/signin', AuthController.signin);
+router.post('/user/signup', AuthController.getStates);
+
+router.get('/user/me', UserController.info);
+router.put('/user/me', UserController.editAction);
+
+router.get('/categories', AdController.getCategories);
+
+router.get('/ad/add', AdController.addAction);
+router.get('/ad/list', AdController.getList);
+router.get('/ad/item', AdController.getItem);
+router.get('/ad/:id', AdController.editAction);
+
+
 module.exports = router;
