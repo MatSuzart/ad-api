1dfbdf90bd7cc43abf8d3d557f1c732334014f68
MatheusSuzart <mathsuzart@outlook.com>
2022-05-03 00:02:37 -0300

    authentication

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const Auth = require('../middleware/Auth');
+
 const AuthController = require('./controllers/AuthController');
 const AdController = require('./controllers/AdController');
 const UserController = require('./controllers/UserController');
@@ -14,15 +16,15 @@ router.get('/states', UserController.getStates);
 router.post('/user/signin', AuthController.signin);
 router.post('/user/signup', AuthController.getStates);
 
-router.get('/user/me', UserController.info);
-router.put('/user/me', UserController.editAction);
+router.get('/user/me', Auth.private, UserController.info);
+router.put('/user/me', Auth.private, UserController.editAction);
 
 router.get('/categories', AdController.getCategories);
 
-router.get('/ad/add', AdController.addAction);
+router.get('/ad/add',Auth.private, AdController.addAction);
 router.get('/ad/list', AdController.getList);
 router.get('/ad/item', AdController.getItem);
-router.get('/ad/:id', AdController.editAction);
+router.get('/ad/:id', Auth.private, AdController.editAction);
 
 
 module.exports = router;
