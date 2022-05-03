b547c692c07e7dfca2ed1cab6347f49a9ace5dbf
MatheusSuzart <mathsuzart@outlook.com>
2022-05-02 23:06:57 -0300

    controller

--- src/controllers/AdController.js
+++ src/controllers/AdController.js
@@ -1,3 +1,14 @@
 module.exports = {
-    
+    getCategories: async (req, res)=>{
+
+    },
+    addAction: async(req, res)=>{
+
+    },
+    getItem: async (req, res)=>{
+
+    },
+    getAction: async (req, res)=>{
+
+    },
 };
--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,3 +1,8 @@
 module.exports = {
-    
+    singin: async(req, res)=>{
+
+    },
+    singup: async(req, res)=>{
+        
+    }
 };
