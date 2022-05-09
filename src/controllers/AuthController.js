295a39facbe6c890c2fb1393e2168ab1adfe312f
MatheusSuzart <mathsuzart@outlook.com>
2022-05-08 22:31:54 -0300

    authcontroller-ended 1

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,4 +1,9 @@
+const mongoose = require('mongoose');
 const { validatorResult, matchedData } = require('express-validator');
+
+const User = require('../moldes/User');
+const State = require('../models/State');
+
 module.exports = {
     singin: async(req, res)=>{
 
@@ -9,6 +14,35 @@ module.exports = {
             res.json({error: erros.mapped()});
             return;
         }   
+
+        const data = matchedData(req);
+
+        const user = await User.findOne({
+            email: data.email
+        });
+
+        if(user){
+            res.json({
+                error: {email:{msg:'EMAIL JÁ EXISTE'}}
+            });
+            return;
+        }
+        if(mongoose.Types.ObjectId.isValid(data.state)){
+        const stateItem = await State.findById(data.state);
+        if(!stateItem){
+            res.json({
+                error:{state:{msg: 'ESTADO NÃO EXISTE'}}
+            });
+            return;
+        }
+    }else{
+        res.json({
+            error:{state:{msg: 'CODIGO DE ESTADO INVALIDO'}}
+        });
+        return;
+    }  
+
+
 
         res.json({tudocerto: true});
     }
