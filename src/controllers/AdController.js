fafaed55f5b580b9f080aee8bab191843eab4694
MatheusSuzart <mathsuzart@outlook.com>
2022-05-14 02:56:02 -0300

    ad-ended

--- src/controllers/AdController.js
+++ src/controllers/AdController.js
@@ -1,3 +1,7 @@
+const User = require("../models/User");
+const Category = require("../models/Category");
+const Ad = require("../models/Ad");
+
 module.exports = {
     getCategories: async (req, res)=>{
         const cats = await Category.find();
@@ -15,6 +19,34 @@ module.exports = {
         res.json({categories});
     },
     addAction: async(req, res)=>{
+        let {title, price, priceng, desc, cat, token} = req.body;
+        const user = await User.findOny({token: token}).exec();
+
+
+        if(!title || !cat){
+            res.json({error: 'TITULO OU CATEGORIA NÃO FORAM PREENCHIDOS'});
+            return;
+        }
+
+        if(price){
+            price = price.replace('.','').replace(',','.').replace('R$  ','');
+
+            price = parseFloat(price);
+        }else {
+            price = 0;
+        }
+
+        const newAd = new Ad();
+        newAd.status = true;
+        newAd.idUser = user._id;
+        newAd.state = user.state;
+        newAd.dateCreated = new Date();
+        newAd.title = title;
+        newAd.category = cat;
+        newAd.price = price;
+        newAd.priceNegotiable = (priceneg=='true') ? true : false;
+        newAd.description = desc;
+        newAd.views = 0;
 
     },
     getItem: async (req, res)=>{
